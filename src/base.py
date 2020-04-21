62cb5df14dcf0431408342e64f3e6e666ff31c61
Ganesh B <ganeshsurfs@gmail.com>
2020-04-22 00:07:46 +0530

    Returning results from all functions to next

--- src/base.py
+++ src/base.py
@@ -5,8 +5,6 @@ from sys import path
 path.append('./')
 
 from .sharedbase import SharedBase
-from .tasks import Tasks
-
 
 class WorkflowBase():
 
@@ -245,83 +243,3 @@ class WorkflowBase():
                 tasks[ik] = inst.tasks.get(ik)
 
         return tasks
-
-
-class Tasks(WorkflowBase):
-
-    def add_plugin(self, plugin_inst):
-        pass
-
-    def merge(self, inst, shared=False, clash_prefix=None):
-
-        if shared == True:
-            self.shared_tasks.tasks = self._merge(
-                self.shared_tasks.tasks, inst, shared, clash_prefix
-            )
-
-        elif shared == False:
-            self.tasks = self._merge(
-                self.tasks, inst, shared, clash_prefix
-            )
-
-    def run(self, tasks):
-
-        print("Workflow task list provided being instantiated: ", str(tasks))
-        print("Workflow has tasks: ", str(self.tasks.keys()))
-        print(
-            "Workflow has shared tasks: ", str(
-                self.shared_tasks.tasks.keys()
-            )
-        )
-        result = []
-
-        if isinstance(tasks, str):
-            # Iterate task through single task
-            result.append(self.run_task(tasks))
-
-        elif isinstance(tasks, list):
-            # Iterate task through tasks
-            [result.append(self.run_task(task_)) for task_ in tasks]
-
-        else:
-            print("No workflow or task available to run")
-
-        return result
-
-
-def workflow(*workflow_args, **workflow_kwargs):
-
-    def get_decorator(function_):
-        # print("get_decorator: Decorator init ", "workflow_args: ", workflow_args, "workflow_kwargs: ", workflow_kwargs)
-        # print("get_decorator: ", function_)
-
-        def order_tasks(*function_args, **function_kwargs):
-
-            # print("Workflow order_tasks: Decorator init ", "function_args: ", function_args, "function_kwargs: ", function_kwargs)
-            t = workflow_kwargs.get("task_instance")
-            if not t:
-                raise Exception("Task instance not provided")
-
-            # Check before/after middlewares args and kwargs number and validity
-            args_normal = t.clean_args(
-                function_, function_args, function_kwargs
-            )
-
-            if not args_normal:
-                raise Exception("Args and KwArgs do not match")
-
-            # print((function_, function_args, function_kwargs, workflow_args, workflow_kwargs))
-            t.set_task(
-                function_, function_args, function_kwargs,
-                workflow_args, workflow_kwargs
-            )
-
-            print(
-                "Workflow order_tasks - Task added: ",
-                workflow_kwargs.get("name")
-            )
-        return order_tasks
-    return get_decorator
-
-
-__all__ = ["Tasks", "workflow"]
---
+++ src/tasks.py
@@ -0,0 +1,51 @@
+# # Project Workflow
+
+
+from sys import path
+path.append('./')
+from .base import WorkflowBase
+
+class Tasks(WorkflowBase):
+
+    def add_plugin(self, plugin_inst):
+        pass
+
+    def merge(self, inst, shared=False, clash_prefix=None):
+
+        if shared == True:
+            self.shared_tasks.tasks = self._merge(
+                self.shared_tasks.tasks, inst, shared, clash_prefix
+            )
+
+        elif shared == False:
+            self.tasks = self._merge(
+                self.tasks, inst, shared, clash_prefix
+            )
+
+    def run(self, tasks):
+
+        print("Workflow task list provided being instantiated: ", str(tasks))
+        print("Workflow has tasks: ", str(self.tasks.keys()))
+        print(
+            "Workflow has shared tasks: ", str(
+                self.shared_tasks.tasks.keys()
+            )
+        )
+        result = []
+
+        if isinstance(tasks, str):
+            # Iterate task through single task
+            result.append(self.run_task(tasks))
+
+        elif isinstance(tasks, list):
+            # Iterate task through tasks
+            [result.append(self.run_task(task_)) for task_ in tasks]
+
+        else:
+            print("No workflow or task available to run")
+
+        return result
+
+
+__all__ = ["Tasks"]
+
