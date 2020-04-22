24add252bfc6c838722cb648d0a9b8e5670e0a60
Ganesh B <ganeshsurfs@gmail.com>
2020-04-22 15:03:28 +0530

    Test structure addition

--- test/test_taskcontrol.py
+++ test/test_taskcontrol.py
@@ -6,41 +6,74 @@ import pytest
 # decorator creates instance tasks
 # decorator creates shared tasks
 class TestDecorator():
-    pass
+
+    def test_create_task(self):
+        pass
+
+    def test_create_instance_task(self):
+        pass
+
+    def test_create_shared_task(self):
+        pass
 
 
 # decorator runs instance single tasks
 # decorator runs instance multiple tasks
 # decorator runs all instance tasks
 class TestTaskRunner():
-    pass
+
+    def test_run_single_instance_task(self):
+        pass
+
+    def test_run_multiple_instance_task(self):
+        pass
+
+    def test_run_all_instance_task(self):
+        pass
 
 
 # decorator runs shared single task
 # decorator runs shared multiple tasks
 # decorator runs shared all tasks
 class TestSharedTaskRunner():
-    pass
+    def test_run_single_shared_tasks(self):
+        pass
 
+    def test_run_single_multiple_tasks(self):
+        pass
+
+    def test_run_single_all_shared_tasks(self):
+        pass
 
 # decorator runs a mix of instance and shared tasks
 class TestAnyTaskRunner():
-    pass
 
+    def test_any_type_task(self):
+        pass
 
 # middlewares can be invoked with arguments and keyword arguments
 # middlewares can be invoked and returns (error or next value) after invocation
 # middlewares can be invoked and returns (error or next value) after invocation
 class TestMiddlewares():
-    pass
 
+    def test_run_middlewares(self):
+        pass
+
+    def test_run_single_middleware(self):
+        pass
 
 # functions can be invoked with arguments and keyword arguments
 # functions can be invoked and returns (error or next value) after invocation
 # functions can be invoked and returns (error or next value) after invocation
 class TestFunctions():
-    pass
+    def test_function_invocation_with_arguments(self):
+        pass
 
+    def test_function_invocation_returns(self):
+        pass
+
+    def test_function_invocation_error_returns(self):
+        pass
 
 # middlewares can be invoked and can access results context of all previously invoked functions
 class TestMiddlewareAccessContext():
