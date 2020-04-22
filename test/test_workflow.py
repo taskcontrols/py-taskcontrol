cd0bfa70b8f01c9564c5240d5d51be3cd7afaaf7
Ganesh B <ganeshsurfs@gmail.com>
2020-04-22 13:54:16 +0530

    Redo of files

---
+++ test/test_workflow.py
@@ -0,0 +1,40 @@
+import pytest
+
+
+# decorator applied on a function and function invocation creates the task
+# decorator creates tasks only on function invocation
+# decorator creates instance tasks
+# decorator creates shared tasks
+
+
+# decorator runs instance single tasks
+# decorator runs instance multiple tasks
+# decorator runs all instance tasks
+
+
+# decorator runs shared single task
+# decorator runs shared multiple tasks
+# decorator runs shared all tasks
+
+
+# decorator runs a mix of instance and shared tasks
+
+
+# middlewares can be invoked with arguments and keyword arguments
+# middlewares can be invoked and returns (error or next value) after invocation
+# middlewares can be invoked and returns (error or next value) after invocation
+
+
+# functions can be invoked with arguments and keyword arguments
+# functions can be invoked and returns (error or next value) after invocation
+# functions can be invoked and returns (error or next value) after invocation
+
+
+# middlewares can be invoked and can access results context of all previously invoked functions
+# functions can be invoked and can access results context of all previously invoked functions
+
+
+# middlewares return results
+# functions return results
+# task runs return results and correct numbers
+
