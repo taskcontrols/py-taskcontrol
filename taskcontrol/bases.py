7b8487d0a6fdda970868225caf9b79e22ebaca8b
Ganesh Bhat <ganeshsurfs@gmail.com>
2020-05-07 17:08:14 +0530

    Structure for concurrency base

--- taskcontrol/bases.py
+++ taskcontrol/bases.py
@@ -1,6 +1,8 @@
 # # Project Workflow
 # Add support for Concurrency
 
+from .pubsub import ConcurencyBase, LoggerBase
+
 
 class SharedBase():
 
@@ -89,45 +91,6 @@ class SharedBase():
         if not SharedBase.__instance:
             SharedBase()
         return SharedBase.__instance
-
-
-class ConcurencyBase():
-
-    # asynchronous, needs_join
-    def mthread_run(self, function, options):
-        from threading import Thread
-        result = None
-        worker = Thread(target=function, daemon=True, args=(
-            *options.get("args"), result), kwargs={**options.get("kwargs")})
-        worker.setDaemon(True)
-        worker.start()
-        if options.get("needs_join"):
-            worker.join()
-        return worker, result
-
-    # asynchronous, needs_join
-    def mprocess_run(self, function, options):
-        from multiprocessing import Process, Array
-        # Apply Array for share with multiple treads
-        # check need here. Create a common one outside by user
-        worker = Process(target=function, args=(
-            *options.get("args"),), kwargs={**options.get("kwargs")})
-        worker.start()
-        if options.get("needs_join"):
-            result = worker.join()
-        return worker, result
-
-
-class LoggerBase():
-
-    def create(self):
-        pass
-
-    def log(self, level, message):
-        pass
-
-    def remove(self):
-        pass
 
 
 class PluginsBase(SharedBase, ConcurencyBase):
--- taskcontrol/pubsub.py
+++ taskcontrol/pubsub.py
@@ -2,6 +2,45 @@
 # TODO: Create structure
 
 
+class ConcurencyBase():
+
+    # asynchronous, needs_join
+    def mthread_run(self, function, options):
+        from threading import Thread
+        result = None
+        worker = Thread(target=function, daemon=True, args=(
+            *options.get("args"), result), kwargs={**options.get("kwargs")})
+        worker.setDaemon(True)
+        worker.start()
+        if options.get("needs_join"):
+            worker.join()
+        return worker, result
+
+    # asynchronous, needs_join
+    def mprocess_run(self, function, options):
+        from multiprocessing import Process, Array
+        # Apply Array for share with multiple treads
+        # check need here. Create a common one outside by user
+        worker = Process(target=function, args=(
+            *options.get("args"),), kwargs={**options.get("kwargs")})
+        worker.start()
+        if options.get("needs_join"):
+            result = worker.join()
+        return worker, result
+
+
+class LoggerBase():
+
+    def create(self):
+        pass
+
+    def log(self, level, message):
+        pass
+
+    def remove(self):
+        pass
+
+
 # Inherit shared and logging
 class ActionsBase():
 
