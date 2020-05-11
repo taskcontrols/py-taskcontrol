8ae9eba4d88629d08646397f4b16b5522e3dc993
Ganesh Bhat <ganeshsurfs@gmail.com>
2020-05-11 19:28:31 +0530

    Minor changes to Concurrency base implementation

--- taskcontrol/concurrency.py
+++ taskcontrol/concurrency.py
@@ -13,45 +13,54 @@ class ConcurencyBase():
 
     # asynchronous, needs_join
     def mthread_run(self, function, options):
-        # # args, kwargs, share_data, needs_join
+        # # args, kwargs, share_value, needs_join
         # Consider adding thread alive, and other method options access to functions
         from threading import Thread
         result = None
         if type(options) == dict:
-            share_data = options.get("share_data")
+            share_value = options.get("share_value")
         worker = Thread(
             target=function,
             daemon=True,
             args=(*options.get("args"), ),
-            kwargs={**options.get("kwargs"),
-                    "share_data": share_data, "result": result}
+            kwargs={**options.get("kwargs")}
         )
         worker.setDaemon(True)
         worker.start()
         if options.get("needs_join"):
             worker.join()
-        return {"worker": worker, "result": result, "share_data": share_data}
+        return {"worker": worker, "result": result, "share_value": share_value}
 
     # asynchronous, needs_join
-    def mprocess_run(self, function, options): 
+    def mprocess_run(self, function, options):
         # options structure
         # args, kwargs, share_data, share_array, needs_join
 
         # Consider adding process alive, and other method options access to functions
-        from multiprocessing import Process, Array, Value
+        from multiprocessing import Process
         result = None
-        sa = options.get("share_array")
-        sv = options.get("share_data")
-        if sa == None:
-            sa = []
-        # check need here. Create a common one outside by user
+
+        # # Check need here. Create a common one outside by user
+        # # Consider if you want to handle this
+        # sv = options.get("share_value")
+        # sa = options.get("share_array")
+        # sq = options.get("share_queue")
+        # spi = options.get("share_pipe")
+        # sl = options.get("share_lock")
+        # srl = options.get("share_rlock")
+        # sm = options.get("share_manager")
+        # spl = options.get("share_pool")
+        # sc = options.get("share_connection")
+        # se = options.get("share_event")
+        # ss = options.get("share_semaphore")
+        # ssb = options.get("share_bounded_semaphore")
+
         worker = Process(
             target=function,
             args=(*options.get("args"), ),
-            kwargs={**options.get("kwargs"),
-                    "share_array": sa, "share_value": sv}
+            kwargs={**options.get("kwargs")}
         )
         worker.start()
         if options.get("needs_join"):
             result = worker.join()
-        return {"worker": worker, "result": result, "share_array": sa, "share_data": sv}
+        return {"worker": worker, "result": result}
