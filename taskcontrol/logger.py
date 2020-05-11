a4b0b3483b267b4fbac609d1d14ed73d52a2753b
Ganesh Bhat <ganeshsurfs@gmail.com>
2020-05-11 19:53:52 +0530

    Minor changes to logger base implementation

--- taskcontrol/logger.py
+++ taskcontrol/logger.py
@@ -7,12 +7,25 @@ class LoggerBase():
         self.logger = logging.Logger(name)
         # self.format = None
         # self.handler = None
+        # del implementation fn
+        self.delt = lambda x: x
+        # delete implementation fn
+        self.delete = lambda x: x
 
     def create(self):
         pass
-        
+
     def log(self, level, message):
         pass
 
-    def remove(self):
+    def __del__(self):
+        # self.delt(1)
+        pass
+
+    def __delete__(self, instance):
+        # self.logger = None
+        # self.format = None
+        # self.handler = None
+
+        # self.delete(1)
         pass
