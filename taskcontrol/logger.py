16b086f9f690827cb83f6d4771f70ac89136144d
Ganesh Bhat <ganeshsurfs@gmail.com>
2020-05-11 19:41:27 +0530

    Minor changes to logger base implementation

--- taskcontrol/logger.py
+++ taskcontrol/logger.py
@@ -2,9 +2,15 @@
 
 class LoggerBase():
 
+    def __init__(self, name):
+        import logging
+        self.logger = logging.Logger(name)
+        # self.format = None
+        # self.handler = None
+
     def create(self):
         pass
-
+        
     def log(self, level, message):
         pass
 
