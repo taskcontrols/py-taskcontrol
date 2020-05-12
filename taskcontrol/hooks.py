162d731ba59e3cc41ccf415ab42333126b215b1a
Ganesh Bhat <ganeshsurfs@gmail.com>
2020-05-12 20:21:04 +0530

    minor change in bases file for auth function for hooks

--- taskcontrol/hooks.py
+++ taskcontrol/hooks.py
@@ -10,32 +10,67 @@ class HooksBase():
 
     def __init__(self):
         self.get_hooks, self.set_hooks = self.hooks_closure()
-    
+
     def hooks_closure(self):
         # list of registered web hooks
         hooks = []
 
         def get_hooks():
+            # TODO: Add Logger
+
+            # TODO: Add Authentication
+            # if not is_authenticated():
+            #     raise Exception("Not authenticated")
             pass
 
         def set_hooks():
+            # TODO: Add Logger
+
+            # TODO: Add Authentication
+            # if not is_authenticated():
+            #     raise Exception("Not authenticated")
             pass
 
         return {"get_hooks": get_hooks, "set_hooks": set_hooks}
 
     def hook_state(self):
+        # TODO: Add Logger
+
+        # TODO: Add Authentication
+        # if not is_authenticated():
+        #     raise Exception("Not authenticated")
         pass
 
     def service_run(self):
+        # TODO: Add Logger
+
+        # TODO: Add Authentication
+        # if not is_authenticated():
+        #     raise Exception("Not authenticated")
         pass
 
     def service_stop(self):
+        # TODO: Add Logger
+
+        # TODO: Add Authentication
+        # if not is_authenticated():
+        #     raise Exception("Not authenticated")
         pass
 
     def register_hook(self):
+        # TODO: Add Logger
+
+        # TODO: Add Authentication
+        # if not is_authenticated():
+        #     raise Exception("Not authenticated")
         pass
 
     def register_receiver(self):
+        # TODO: Add Logger
+
+        # TODO: Add Authentication
+        # if not is_authenticated():
+        #     raise Exception("Not authenticated")
         pass
 
     def send(self):
