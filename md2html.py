63b393317c1264784a3fb7d4be0606d0da9a31b4
Ganesh Bhat <ganeshsurfs@gmail.com>
2020-05-13 16:22:06 +0530

    Minor changes to docs and html convertor

--- md2html.py
+++ md2html.py
@@ -1,9 +1,15 @@
 from os import listdir
-from os.path import isfile, join
+from os.path import isfile, join, exists
 import subprocess
+import markdown
 
 paths = ["./docs/users", "./docs/apis", "./docs/arch"]
 for p in paths:
     files = [f for f in listdir(p) if isfile(join(p, f))]
     for i in files:
-        subprocess.run(["md-to-html", "--input", p+"/"+i, "--output", p+"/"+i.split(".md")[0] + ".html"])
+        with open(p+"/"+i) as f:
+            html = markdown.markdown(f.read())
+            f.close()
+            with open(p+"/"+i.split(".md")[0]+".html", "w+") as c:
+                c.write(html)
+
