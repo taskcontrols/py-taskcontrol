c9954f46000a9139a32c37580b6eb50c855bdf66
Ganesh Bhat <ganeshsurfs@gmail.com>
2020-06-11 20:11:25 +0530

    Adding code for registering, removing events/actions, and listeners

--- taskcontrol/actions.py
+++ taskcontrol/actions.py
@@ -23,12 +23,12 @@ class ActionsBase(ClosureBase):
         self.getter, self.setter, self.deleter = self.class_closure(
             actions=[], action_listeners=[])
 
-    def action_state(self):
+    def action_state(self, key):
         """
         Description of action_state
 
         Args:
-            self (undefined):
+            key (string):
 
         """
         # TODO: Add Logger
@@ -36,14 +36,14 @@ class ActionsBase(ClosureBase):
         # TODO: Add Authentication
         # if not is_authenticated():
         #     raise Exception("Not authenticated")
-        pass
+        return self.getter("actions", key)
 
-    def register_event(self):
+    def register_event(self, event_object):
         """
         Description of register_event
 
         Args:
-            self (undefined):
+            event_object (dict):
 
         """
         # TODO: Add Logger
@@ -51,29 +51,16 @@ class ActionsBase(ClosureBase):
         # TODO: Add Authentication
         # if not is_authenticated():
         #     raise Exception("Not authenticated")
-        pass
-
-    def register_listener(self):
-        """
-        Description of register_listener
-
-        Args:
-            self (undefined):
-
-        """
-        # TODO: Add Logger
-
-        # TODO: Add Authentication
-        # if not is_authenticated():
-        #     raise Exception("Not authenticated")
-        pass
-
-    def unregister_listener(self):
+        if self.setter("actions", event_object):
+            return True
+        return False
+    
+    def unregister_event(self, event_object):
         """
         Description of unregister_listener
 
         Args:
-            self (undefined):
+            event_object (dict):
 
         """
         # TODO: Add Logger
@@ -81,14 +68,50 @@ class ActionsBase(ClosureBase):
         # TODO: Add Authentication
         # if not is_authenticated():
         #     raise Exception("Not authenticated")
-        pass
+        if self.setter("actions", event_object):
+            return True
+        return False
 
-    def message(self):
+    def register_listener(self, listener_object):
+        """
+        Description of register_listener
+
+        Args:
+            listener_object (dict):
+
+        """
+        # TODO: Add Logger
+
+        # TODO: Add Authentication
+        # if not is_authenticated():
+        #     raise Exception("Not authenticated")
+        if self.setter("action_listeners", listener_object):
+            return True
+        return False
+
+    def unregister_listener(self, listener_object):
+        """
+        Description of unregister_listener
+
+        Args:
+            listener_object (dict):
+
+        """
+        # TODO: Add Logger
+
+        # TODO: Add Authentication
+        # if not is_authenticated():
+        #     raise Exception("Not authenticated")
+        if self.setter("action_listeners", listener_object):
+            return True
+        return False
+
+    def message(self, msg_object):
         """
         Description of message
 
         Args:
-            self (undefined):
+            msg_object (dict):
 
         """
         # TODO: Add Logger
@@ -96,14 +119,15 @@ class ActionsBase(ClosureBase):
         # TODO: Add Authentication
         # if not is_authenticated():
         #     raise Exception("Not authenticated")
-        pass
+        action = self.getter("actions", msg_object.get("key"))
+        action.send(msg_object.get("message"))
 
-    def listen(self):
+    def listen(self, options):
         """
         Description of listen
 
         Args:
-            self (undefined):
+            options (dict):
 
         """
         # TODO: Add Logger
@@ -111,4 +135,6 @@ class ActionsBase(ClosureBase):
         # TODO: Add Authentication
         # if not is_authenticated():
         #     raise Exception("Not authenticated")
-        pass
+        action = self.getter("actions", options.get("key"))
+        action.listen(options)
+
