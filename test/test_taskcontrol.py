9d83fbb761284ffea38221b444cf1a195b925c6f
Ganesh Bhat <ganeshsurfs@gmail.com>
2020-06-20 01:50:53 +0530

    Adding tests for usage until 0207

--- test/test_taskcontrol.py
+++ test/test_taskcontrol.py
@@ -1636,58 +1636,6 @@ class TestTaskRunner():
         assert t.shared.getter("tasks", "taskname") == []
         assert type(t.shared.getter("tasks", "taskname")) == list
 
-    def test_2_7_doesnot_run_single_instance_multiple_tasks(self):
-        def middleware(ctx, result, k, c, d, **kwargs):
-            print("Running my Middleware Function: test - task items", k, c, d, kwargs)
-            return 207
-
-        t = Tasks()
-
-        @workflow(
-            name="taskname", task_order=1, task_instance=t,
-            shared=False, args=[1, 2], kwargs={},
-            before=[{
-                "function": middleware, "args": [11, 12], "kwargs": {"d": "Before Testing message Middleware "},
-                "options": {"error": "next", "error_next_value": ""}
-            }],
-            after=[{
-                "function": middleware, "args": [13, 14], "kwargs": {"d": "After Middleware Testing message"},
-                "options": {"error": "error_handler", "error_next_value": "value", "error_handler": lambda err, value: (err, value)}
-            }],
-            log=False
-        )
-        def taskname(ctx, result, a, b):
-            print("Running my task function: taskone", a, b)
-            return 207
-
-        @workflow(
-            name="tasktwo", task_order=1, task_instance=t,
-            shared=False, args=[1, 2], kwargs={},
-            before=[{
-                "function": middleware, "args": [11, 12], "kwargs": {"d": "Before Testing message Middleware "},
-                "options": {"error": "next", "error_next_value": ""}
-            }],
-            after=[{
-                "function": middleware, "args": [13, 14], "kwargs": {"d": "After Middleware Testing message"},
-                "options": {"error": "error_handler", "error_next_value": "value", "error_handler": lambda err, value: (err, value)}
-            }],
-            log=False
-        )
-        def tasktwo(ctx, result, a, b):
-            print("Running my task function: taskone", a, b)
-            return 207
-
-        result = t.run(tasks="taskname")
-
-        result = t.run(tasks="tasktwo")
-
-        result = t.run(tasks="taskname")
-
-        result = t.run(tasks=["shared:taskname", "shared:tasktwo"])
-
-        t.shared.deleter("tasks", 'taskname')
-        t.shared.deleter("tasks", 'tasktwo')
-
     def test_2_7_run_all_instance_task(self):
         def middleware(ctx, result, k, c, d, **kwargs):
             print("Running my Middleware Function: test - task items", k, c, d, kwargs)
