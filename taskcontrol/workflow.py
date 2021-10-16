6c7bb1269f6618950bc901ef22971834491b94cb
Ganesh Bhat <ganeshsurfs@gmail.com>
2021-10-16 13:27:52 +0530

    Minor changes to workflow file

--- taskcontrol/workflow.py
+++ taskcontrol/workflow.py
@@ -1,9 +1,11 @@
 # # Project Workflow
+
 from sys import path
 path.append('./')
 
-from .interfaces import AuthenticationBase, SocketsBase, HooksBase, SQLBase, LogsBase, TimeBase, CommandBase
+
 from .bases import WorkflowBase, PluginsBase, LogBase, TimerBase, ConcurencyBase, AuthBase, UtilsBase, Queues, Actions, Events, Hooks, Sockets, CommandsBase
+from .interfaces import AuthenticationBase, SocketsBase, HooksBase, SQLBase, LogsBase, TimeBase, CommandBase
 
 
 
@@ -80,8 +82,10 @@ def workflow(*workflow_args, **workflow_kwargs):
             if type(workflow_kwargs.get("kwargs")) != dict:
                 workflow_kwargs["kwargs"] = {}
 
-            workflow_kwargs.update({"args": workflow_kwargs.get("args")})
-            workflow_kwargs.update({"kwargs": workflow_kwargs.get("kwargs")})
+            workflow_kwargs.update({
+                "args": workflow_kwargs.get("args", []),
+                "kwargs": workflow_kwargs.get("kwargs", {})
+            })
 
             t = workflow_kwargs.get("task_instance")
             if not t:
@@ -90,11 +94,13 @@ def workflow(*workflow_args, **workflow_kwargs):
             if not workflow_kwargs.get("name"):
                 raise TypeError("Name Argument not provided")
 
-            workflow_kwargs["before"] = workflow_kwargs.get("before", [])
-            workflow_kwargs["after"] = workflow_kwargs.get("after", [])
-            workflow_kwargs["shared"] = workflow_kwargs.get("shared", False)
-            workflow_kwargs["options"] = workflow_kwargs.get("options", {})
-            workflow_kwargs["log"] = workflow_kwargs.get("log", False)
+            workflow_kwargs.update({
+                "before": workflow_kwargs.get("before", []),
+                "after": workflow_kwargs.get("after", []),
+                "shared": workflow_kwargs.get("shared", False),
+                "options": workflow_kwargs.get("options", {}),
+                "log": workflow_kwargs.get("log", False)
+            })
 
             args = []
             if len(workflow_kwargs["args"]) > 0:
@@ -115,19 +121,18 @@ def workflow(*workflow_args, **workflow_kwargs):
             if args_normal == None or args_normal == False:
                 raise Exception("Args and Kwargs do not match")
 
-            # function_, args, kwargs,
-            #     workflow_args, workflow_kwargs
+            # function_, args, kwargs, workflow_args, workflow_kwargs
 
-            fn_task = {}
-            fn_task["name"] = workflow_kwargs.get("name")
-            fn_task["task_order"] = workflow_kwargs.get("task_order")
-            fn_task["function"] = function_
-            fn_task["args"] = workflow_kwargs.get("args")
-            fn_task["kwargs"] = workflow_kwargs.get("kwargs")
-            fn_task["workflow_args"] = workflow_args
-            fn_task["workflow_kwargs"] = workflow_kwargs
-            fn_task["log"] = workflow_kwargs.get("log")
-
+            fn_task = {
+                "name": workflow_kwargs.get("name"),
+                "task_order": workflow_kwargs.get("task_order"),
+                "function": function_,
+                "args": workflow_kwargs.get("args"),
+                "kwargs": workflow_kwargs.get("kwargs"),
+                "workflow_args": workflow_args,
+                "workflow_kwargs": workflow_kwargs,
+                "log": workflow_kwargs.get("log")
+            }
             t.setter("tasks", fn_task, t)
 
             # print("Workflow add_tasks - Task added: ",
