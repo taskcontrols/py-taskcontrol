5e7ecb4c43b4a8622debd0ca40ac939e0964ad02
Ganesh Bhat <ganeshsurfs@gmail.com>
2021-10-17 22:07:08 +0530

    Minor changes to utils file in timer and logbase classes

--- taskcontrol/utils.py
+++ taskcontrol/utils.py
@@ -347,43 +347,41 @@ class ConcurencyBase(UtilsBase):
         return None
 
 
-class TimerBase(TimeBase, ClosureBase, UtilsBase):
+class TimerBase(UtilsBase, TimeBase):
 
-    def __init__(self, options, timers={}):
-        super()
-
-        if not options and type(options) != dict:
-            raise TypeError("Options not provided")
-
-        self.getter, self.setter, self.deleter = self.class_closure(
-            timers=timers)
+    def __init__(self, timers={}):
+        self.v = ["name", "timer"]
+        super.__init__("timers",
+                       validations={"add": self.v, "create": self.v,
+                                    "update": self.v, "delete": ["name"]},
+                       timers=timers)
 
     def time(self, options):
-
         # options object expected
-        # {"name":"name", "logger": "", "format": ""}
+        # {"name":"name", "timer": None}
 
-        logger = options.get("logger")
         t = self.getter("timers", options.get("name"))
+        if not t:
+            raise TypeError("Wrong timer name provided")
 
-        if len(t) > 0:
-            timer = t[0].perf_counter()
-        else:
-            raise TypeError(
-                "Wrong timer name provided. No such timer or multiple names matched")
+        timer = t.get("timer").perf_counter()
 
         if not timer:
             raise ValueError("Did not find timer")
-        if logger:
-            logger.log(timer)
         return timer
 
+    def stop(self, options):
+        pass
 
-class LogBase(LogsBase, ClosureBase, UtilsBase):
 
-    def __init__(self, name, config):
+class LogBase(UtilsBase, LogsBase):
 
-        self.getter, self.setter, self.deleter = self.class_closure(loggers={})
+    def __init__(self, loggers={}):
+        self.v = ["name", "logger", "level", "format", "handlers"]
+        super.__init__("timers",
+                       validations={"add": self.v, "create": self.v,
+                                    "update": self.v, "delete": ["name"]},
+                       loggers=loggers)
 
         # self.setter("loggers", config, self)
         # self.format = None
@@ -395,48 +393,70 @@ class LogBase(LogsBase, ClosureBase, UtilsBase):
         # delete implementation fn (get from config)
         self.delete = lambda x: x
 
-    def create_logger(self, config):
+    def logger_create(self, config):
 
         # Config object expected
         # { "name":"name", "logger":logger, "level": "debug", "format": "",
-        #   "handlers": {"handler": "", "value": ""}, "handlers": [{"handler": "", "value": ""}] }
+        #   "handlers": {"handler": {"type": "file", "file": "filename.log"}, "format": "", "level": logging.INFO},
+        #   "handlers": [{"handler": {"type": "file", "file": "filename.log"}, "format": "", "level": logging.DEBUG}]
+        # }
 
-        logger = self.getter("loggers", config.get("name"))
+        logger = self.fetch(config.get("name"))
+        if not logger:
+            raise ValueError("Logger not present")
 
         # Use config here
         # config contains network info if logging needed to network
-        if len(logger) > 1:
-            raise ValueError(
-                "Number of logger items ({0}) incorrect. Check the logger registeration".format(len(logger)))
-        elif len(logger) == 1:
-            log = logger[0]
-        else:
-            log = logging.getLogger(config.get("name"))[0]
+        log = logging.getLogger(config.get("name"))
+        if not config.get("handlers"):
+            raise TypeError("Handlers Object not provided")
 
-        if config.get("handlers") and type(config.get("handlers")) == list:
-            for i in config.get("handlers"):
-                # {"handler": "FileHandler", "value": None}
-                h = getattr(logging, config.get(i["handler"]))(
-                    config.get(i["value"]))
-                h.setLevel(getattr(logging, config.get("level")))
+        def log_hdlr(cfg):
+            if cfg.get("handler"):
+                h = None
+                if cfg.get("handler").get("type") == "file":
+                    h = logging.FileHandler(cfg.get(
+                        "handler").get("file"), "logging_" + cfg.get("name") + ".log")
+                elif cfg.get("handler").get("type") == "stream":
+                    h = logging.StreamHandler()
+                else:
+                    return False
+                h.setLevel(cfg.get("level", logging.INFO))
+                fmt = logging.Formatter(
+                    cfg.get("format", "%(levelname)s - %(asctime)s - %(name)s - %(message)s"))
+                return h.setFormatter(fmt)
+            return False
+
+        if type(config.get("handlers")) == list:
+            for hdlrs in config.get("handlers"):
+                if type(hdlrs) != dict:
+                    raise ValueError("Error One in One of Configs")
+                h = log_hdlr(hdlrs)
+                if not h:
+                    raise ValueError("Error Two in One of Configs")
                 log.addHandler(h)
-        elif config.get("handlers") and type(config.get("handlers")) == dict:
-            h = getattr(logging, config.get("handler"))(config.get("value"))
-            h.setLevel(getattr(logging, config.get("level")))
+        elif type(config.get("handlers")) == dict:
+            h = log_hdlr(config.get("handler"))
+            if not h:
+                raise ValueError("Error in Config")
             log.addHandler(h)
         else:
-            raise TypeError("Config object handler key error")
+            raise TypeError
 
-        log.setFormatter(log.Formatter(config.get("format")))
-        config["logger"] = log
-        self.setter("loggers", config, self)
+        config.update({"logger": log})
+        u = self.update(config)
+        if u:
+            return self.fetch(config.get("name"))
+        return False
 
-    def delete_logger(self, options):
+    def logger_delete(self, options):
 
         # options object expected
         # {"key":"name", "value": ""}
-
-        self.deleter(options.get("key"), options.get("value"))
+        u = self.deleter(options.get("name"))
+        if u:
+            return True
+        return False
 
     def log(self, options):
         # TODO: Concurrency can be added
@@ -445,13 +465,7 @@ class LogBase(LogsBase, ClosureBase, UtilsBase):
         # options object expected
         # {"name":"name", "level": "debug", "message": ""}
 
-        logger = self.getter("loggers", options.get("name"))
-        if (not len(logger) == 0 or not len(logger) > 1) and logger:
-            log = logger[0]
-        else:
-            raise ValueError(
-                "Logger items ({0}) incorrect. Check logger".format(len(logger)))
-
+        log = self.fetch(options.get("name"))
         level = options.get("level")
         message = options.get("message")
 
