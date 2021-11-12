20b06fdf7d45d68f3186f77e919ed6c7f4f7a11a
Ganesh Bhat <ganeshsurfs@gmail.com>
2021-11-12 10:14:18 +0530

    [TEMP] Minor changes to lib folder and removing bases file

--- taskcontrol/bases.py
+++
@@ -1,5 +0,0 @@
-from taskcontrol.framework.authentication import *
-from taskcontrol.framework.bases import *
-from taskcontrol.framework.orm import *
-from taskcontrol.framework.utils import *
-from taskcontrol.framework.workflows import *
