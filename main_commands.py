a2773aeaeab709d95e34f39aa49c6b75df52a044
Ganesh Bhat <ganeshsurfs@gmail.com>
2021-11-24 11:35:30 +0530

    [TEMP] Adding commits for .run and .Popen of subprocess

--- main_commands.py
+++ main_commands.py
@@ -11,13 +11,13 @@ command = cmd.pop(0)
 # # Will have STDOUT or STDERR
 
 result_popen = c.execute(command, mode="subprocess_popen", stdin_mode=True, options={"args": cmd, "stdin_input": "touch tests"})
-print("RUNNING RESULT FOR POPEN:\n")
+print("RUNNING RESULT FOR POPEN [Popen Object - key:'stdout' type(_io.TextIOWrapper)]:\n")
 for l in result_popen.__dict__.get("_stdout_buff"):
     for i in l.split("\n"):
         print(i)
 
 result_run = c.execute(command, mode="subprocess_run", stdin_mode=True, options={"args": cmd, "stdin_input": "touch tests"})
-print("RUNNING RESULT FOR RUN:\n")
+print("RUNNING RESULT FOR RUN [CompletedProcess Object - key:'stdout' type(str)]:\n")
 print(result_run.__dict__.get("stdout"))
 
 # TODO: Locks up in Windows(x64)
